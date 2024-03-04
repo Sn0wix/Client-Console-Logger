5286ada87da6265a0845ed06b0d2f967c0e71dec
Sn0wix_ <sn0wix@proton.me>
2024-03-04 17:10:13 +0100

    upload

---
+++ src/main/java/net/sn0wix_/clientConsoleLogger/ClientConsoleLogger.java
@@ -0,0 +1,15 @@
+package net.sn0wix_.clientConsoleLogger;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientConsoleLogger implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("client-console-logger");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
